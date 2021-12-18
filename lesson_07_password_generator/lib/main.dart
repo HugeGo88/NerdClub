af2923c11892563979a0e3a4806a4c4f5accb1a3
Hugo Tausch <hugo.tausch@gmail.com>
2021-12-18 18:21:27 +0100

    basic implemantation of the app till homework

--- lesson_07_password_generator/lib/main.dart
+++ lesson_07_password_generator/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:crypt/crypt.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primarySwatch: Colors.indigo,
       ),
       home: const MyHomePage(title: 'Password Generator'),
       debugShowCheckedModeBanner: false,
@@ -30,8 +31,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late TextEditingController _controller;
+  String _password = 'Nichts';
+
+  void _generatePassword(String masterPW) {
+    setState(() {
+      //TODO: Website aus Textfeld lesen
+      _password =
+          Crypt.sha256('web.com', salt: _controller.text).hash.substring(0, 10);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+    //TODO: Delete for release
+    _controller.text = '12345';
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(8.0),
+              //TODO: TextField für Website
+              child: TextField(
+                controller: _controller,
+                autocorrect: false,
+                obscureText: true,
+                enableSuggestions: false,
+                decoration: const InputDecoration(
+                    border: OutlineInputBorder(),
+                    hintText: 'Enter Master Password',
+                    labelText: 'Master Password'),
+              ),
+            ),
+            ElevatedButton(
+              // übergeben von Funktion mit Parameter
+              onPressed: () => _generatePassword(_controller.text),
+              child: Text('Generate Password'),
+            ),
+            //TODO: Text formatieren https://api.flutter.dev/flutter/painting/TextStyle-class.html
+            Text(_password),
+          ],
+        ),
+      ),
+    );
   }
 }
